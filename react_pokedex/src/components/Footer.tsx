cd223ed31ee98047795777006d2a2cb7a8a74469
Evan <evan.rktcisse@gmail.com>
2024-11-19 02:54:35 +0100

    :sparkles: Added Footer

---
+++ react_pokedex/src/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-dark text-light text-center mt-auto py-1">
+        <div className="container">
+          <p>
+            <strong>Pokédex v0.1</strong>
+          </p>
+          <p>
+            Created with 💛 by <strong>Evan</strong>
+          </p>
+          <p>
+            All Pokémon data &copy; Nintendo, Game Freak, and The Pokémon
+            Company.
+            <br />
+            This is a fan-made project for educational and entertainment
+            purposes.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
